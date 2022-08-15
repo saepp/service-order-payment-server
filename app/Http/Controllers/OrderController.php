5422469a9172eebe8cc55cbe9c83eab8ee9209cf
Septian Adhitya <dsrisk911@gmail.com>
2022-08-15 16:36:38 +0700

    add: function index

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -4,9 +4,25 @@ namespace App\Http\Controllers;
 
 use App\Models\Order;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class OrderController extends Controller
 {
+    public function index(Request $request)
+    {
+        $userId = $request->input('user_id');
+        $orders = Order::query();
+
+        $orders->when($userId, function ($query) use ($userId) {
+            return $query->where('user_id', '=', $userId);
+        });
+
+        return response()->json([
+            'status' => 'success',
+            'data' => $orders->get()
+        ]);
+    }
+
     public function create(Request $request)
     {
         $user = $request->input('user');
@@ -18,7 +34,7 @@ class OrderController extends Controller
         ]);
 
         $transactionDetails = [
-            'order_id' => $order->id,
+            'order_id' => $order->id . Str::random(5),
             'gross_amount' => $course['price'],
         ];
 
@@ -45,8 +61,23 @@ class OrderController extends Controller
         ];
 
         $midtransSnapUrl = $this->getMidtransSnapUrl($midtransParams);
-        return $midtransSnapUrl;
-        // return response()->json($order);
+
+        $order->snap_url = $midtransSnapUrl;
+
+        $order->metadata = [
+            'course_id' => $course['id'],
+            'course_price' => $course['price'],
+            'course_name' => $course['name'],
+            'course_thumbnail' => $course['thumbnail'],
+            'course_level' => $course['level'],
+        ];
+
+        $order->save();
+
+        return response()->json([
+            'status' => 'success',
+            'data' => $order
+        ]);
     }
 
     private function getMidtransSnapUrl($params)
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('orders', [OrderController::class, 'create']);
+Route::get('orders', [OrderController::class, 'index']);
